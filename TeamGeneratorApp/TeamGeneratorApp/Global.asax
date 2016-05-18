ea4fc33682303911d4b03647e7b0aec11552765c
mbalic <mario.balic@span.eu>
2016-05-18 18:37:58 +0200

    inital commit

---
+++ TeamGeneratorApp/TeamGeneratorApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamGeneratorApp.MvcApplication" Language="C#" %>
---
+++ TeamGeneratorApp/TeamGeneratorApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamGeneratorApp.Startup))]
+namespace TeamGeneratorApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
