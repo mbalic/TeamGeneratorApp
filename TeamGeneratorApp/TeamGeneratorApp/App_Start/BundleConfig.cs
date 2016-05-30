eccd63f86d567806c5240de8bb1fd079d6de285c
mbalic <mario.balic@span.eu>
2016-05-30 18:30:02 +0200

    added kendo.mvc

--- TeamGeneratorApp/TeamGeneratorApp/App_Start/BundleConfig.cs
+++ TeamGeneratorApp/TeamGeneratorApp/App_Start/BundleConfig.cs
@@ -23,9 +23,22 @@ namespace TeamGeneratorApp
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/kendo/kendo.all.min.js",
+                // "~/Scripts/kendo/kendo.timezones.min.js", // uncomment if using the Scheduler
+                "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
+               "~/Content/kendo/kendo.common-bootstrap.min.css",
+               "~/Content/kendo/kendo.bootstrap.min.css"));
+
+            bundles.IgnoreList.Clear();
+
+
         }
     }
 }
